2779280ab0837461301c387765f1c452180ce10c
Abdullajon <110884479+AbdullajonOdilov@users.noreply.github.com>
2023-06-26 08:21:50 +0500

    Update models.py

--- models.py
+++ models.py
@@ -52,6 +52,6 @@ business_pydantic = pydantic_model_creator(Business, name="Business")
 business_pydanticIn = pydantic_model_creator(
     Business, name="BusinessIn", exclude_readonly=True, exclude=("logo", ))
 
-product_pydantic = pydantic_model_creator(Product, name="Product")
-product_pydanticIn = pydantic_model_creator(
-    Product, name="ProductIn", exclude=("percentage_discount", "id", "product_image", "date_published"))
+# product_pydantic = pydantic_model_creator(Product, name="Product")
+# product_pydanticIn = pydantic_model_creator(
+#     Product, name="ProductIn", exclude=("percentage_discount", "id", "product_image", "date_published"))
